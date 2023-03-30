61751f2c9a641ed184fb14261d8209a9b0b6b0c3
Kateryna Shyurkova <katerina.shyurkova@gmail.com>
2023-03-30 20:07:54 +0300

    add click

---
+++ src/Applay.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function Applay() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
--- src/ButtonContact.js
+++ src/ButtonContact.js
@@ -1,11 +1,20 @@
 import * as React from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+// import Applay from "./Applay";
+function applay(event) {
+  console.log("hello cat");
+}
 
 export default function BasicButtons() {
   return (
     <Stack spacing={2} direction="row" display="block">
-      <Button variant="contained" color="primary" sx={{ color: "white" }}>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ color: "white" }}
+        onClick={applay}
+      >
         Записатися
       </Button>
     </Stack>
