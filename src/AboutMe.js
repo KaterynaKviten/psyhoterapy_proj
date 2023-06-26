235e75607ebf8dc0af6b8e740fb7cf9a3b0e863b
Kateryna Shyurkova <katerina.shyurkova@gmail.com>
2023-06-26 21:22:45 +0300

    added flex

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,14 +2,15 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+
+// import Grid from "@mui/material/Grid";
 // import ButtonContact from "./ButtonContact";
 import ModalContact from "./ModalContact";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "light" ? "#f2f2f2" : "#fff",
   ...theme.typography.body2,
-  padding: "10px 10px 30px 10px ",
+  padding: "30px 10px 30px 10px ",
   margin: "30px 10px 10px 10px ",
   textAlign: "center",
   color: theme.palette.text.secondary,
@@ -17,52 +18,54 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function BasicGrid() {
   return (
-    <Box id="aboutmePage">
-      <Grid
-        container
-        spacing={1}
-        justifyContent="center"
-        direction="row-reverse"
+    <Box
+      id="aboutmePage"
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
+        alignContent: "stretch",
+      }}
+    >
+      <Item
+        sx={{
+          Width: "300px",
+          flex: 1,
+        }}
       >
-        <Grid item sm={6} alignItems="center">
-          <Item>
-            <h1>Про мене</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <ul>
-              <li>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt
-              </li>
-              <li> Excepteur sint occaecat</li>
-              <li>Ut enim ad minim veniam</li>
-              <li>Excepteur sint</li>
-              <li>Lorem ipsum</li>
-            </ul>
-            <ModalContact />
-          </Item>
-        </Grid>
-
-        <Grid item sm={6}>
-          <Item>
-            <img
-              src="./women.jpg"
-              alt="myphoto"
-              style={{
-                width: "50%",
-                borderRadius: "3px",
-              }}
-            ></img>
-          </Item>
-        </Grid>
-      </Grid>
+        <h1 style={{ marginTop: 0 }}>Про мене</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt
+          </li>
+          <li> Excepteur sint occaecat</li>
+          <li>Ut enim ad minim veniam</li>
+          <li>Excepteur sint</li>
+          <li>Lorem ipsum</li>
+        </ul>
+        <ModalContact />
+      </Item>
+      <Item>
+        <img
+          src="./women.jpg"
+          alt="myphoto"
+          style={{
+            width: "300px",
+            borderRadius: "3px",
+          }}
+        ></img>
+      </Item>
     </Box>
   );
 }
--- src/Service.js
+++ src/Service.js
@@ -2,7 +2,8 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+
+// import Grid from "@mui/material/Grid";
 // import ButtonContact from "./ButtonContact";
 import ModalContact from "./ModalContact";
 
@@ -17,68 +18,161 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function BasicGrid() {
   return (
-    <Box id="servisePage">
-      <h1>Послуги</h1>
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        direction="row-reverse"
+    <Box
+      id="servisePage"
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
+        alignContent: "stretch",
+      }}
+    >
+      <Item
+        sx={{
+          Width: "300px",
+          flex: 1,
+        }}
       >
-        <Grid item sm={4} alignItems="center">
-          <Item>
-            <h1>Консультація</h1>
-            <ul>
-              <li>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt
-              </li>
-              <li> Excepteur sint occaecat</li>
-              <li>Ut enim ad minim veniam</li>
-              <li>Excepteur sint</li>
-              <li>Lorem ipsum</li>
-            </ul>
-            <h3>Ціна: 1000 грн</h3>
-            <ModalContact />
-          </Item>
-        </Grid>
-
-        <Grid item sm={4} alignItems="center">
-          <Item>
-            <h1>Сімейна консультація</h1>
-            <ul>
-              <li>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt
-              </li>
-              <li> Excepteur sint occaecat</li>
-              <li>Ut enim ad minim veniam</li>
-              <li>Excepteur sint</li>
-              <li>Lorem ipsum</li>
-            </ul>
-            <h3>Ціна: 1000 грн</h3>
-            <ModalContact />
-          </Item>
-        </Grid>
-
-        <Grid item sm={4} alignItems="center">
-          <Item>
-            <h1>Консультація дитини</h1>
-            <ul>
-              <li>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt
-              </li>
-              <li> Excepteur sint occaecat</li>
-              <li>Ut enim ad minim veniam</li>
-              <li>Excepteur sint</li>
-              <li>Lorem ipsum</li>
-            </ul>
-            <h3>Ціна: 1000 грн</h3>
-            <ModalContact />
-          </Item>
-        </Grid>
-      </Grid>
+        <h2 style={{ marginTop: 0 }}>Консультація</h2>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt
+          </li>
+          <li> Excepteur sint occaecat</li>
+          <li>Ut enim ad minim veniam</li>
+          <li>Excepteur sint</li>
+          <li>Lorem ipsum</li>
+        </ul>
+        <h3>Ціна: 1000 грн</h3>
+        <ModalContact />
+      </Item>
+      <Item
+        sx={{
+          Width: "300px",
+          flex: 1,
+        }}
+      >
+        <h2 style={{ marginTop: 0 }}>Сімейна консультація</h2>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt
+          </li>
+          <li> Excepteur sint occaecat</li>
+          <li>Ut enim ad minim veniam</li>
+          <li>Excepteur sint</li>
+          <li>Lorem ipsum</li>
+        </ul>
+        <h3>Ціна: 1000 грн</h3>
+        <ModalContact />
+      </Item>
+      <Item
+        sx={{
+          Width: "300px",
+          flex: 1,
+        }}
+      >
+        <h2 style={{ marginTop: 0 }}>Консультація дитини</h2>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt
+          </li>
+          <li> Excepteur sint occaecat</li>
+          <li>Ut enim ad minim veniam</li>
+          <li>Excepteur sint</li>
+          <li>Lorem ipsum</li>
+        </ul>
+        <h3>Ціна: 1000 грн</h3>
+        <ModalContact />
+      </Item>
     </Box>
   );
 }
+
+// import * as React from "react";
+// import { styled } from "@mui/material/styles";
+// import Box from "@mui/material/Box";
+// import Paper from "@mui/material/Paper";
+// import Grid from "@mui/material/Grid";
+// // import ButtonContact from "./ButtonContact";
+// import ModalContact from "./ModalContact";
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === "light" ? "#f2f2f2" : "#fff",
+//   ...theme.typography.body2,
+//   padding: "30px 10px 30px 10px ",
+//   margin: "30px 10px 10px 10px ",
+//   textAlign: "center",
+//   color: theme.palette.text.secondary,
+// }));
+
+// export default function BasicGrid() {
+//   return (
+//     <Box id="servisePage">
+//       <h1>Послуги</h1>
+//       <Grid
+//         container
+//         spacing={2}
+//         justifyContent="center"
+//         direction="row-reverse"
+//       >
+//         <Grid item sm={4} alignItems="center">
+//           <Item>
+//             <h1>Консультація</h1>
+//             <ul>
+//               <li>
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor incididunt
+//               </li>
+//               <li> Excepteur sint occaecat</li>
+//               <li>Ut enim ad minim veniam</li>
+//               <li>Excepteur sint</li>
+//               <li>Lorem ipsum</li>
+//             </ul>
+//             <h3>Ціна: 1000 грн</h3>
+//             <ModalContact />
+//           </Item>
+//         </Grid>
+
+//         <Grid item sm={4} alignItems="center">
+//           <Item>
+//             <h1>Сімейна консультація</h1>
+//             <ul>
+//               <li>
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor incididunt
+//               </li>
+//               <li> Excepteur sint occaecat</li>
+//               <li>Ut enim ad minim veniam</li>
+//               <li>Excepteur sint</li>
+//               <li>Lorem ipsum</li>
+//             </ul>
+//             <h3>Ціна: 1000 грн</h3>
+//             <ModalContact />
+//           </Item>
+//         </Grid>
+
+//         <Grid item sm={4} alignItems="center">
+//           <Item>
+//             <h1>Консультація дитини</h1>
+//             <ul>
+//               <li>
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor incididunt
+//               </li>
+//               <li> Excepteur sint occaecat</li>
+//               <li>Ut enim ad minim veniam</li>
+//               <li>Excepteur sint</li>
+//               <li>Lorem ipsum</li>
+//             </ul>
+//             <h3>Ціна: 1000 грн</h3>
+//             <ModalContact />
+//           </Item>
+//         </Grid>
+//       </Grid>
+//     </Box>
+//   );
+// }
